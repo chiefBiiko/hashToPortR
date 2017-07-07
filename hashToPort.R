ca152f114d8978eb7575533062117bf76c6a5cfc
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-07 09:50:30 +0200

    hash2port

---
+++ hashToPort.R
@@ -0,0 +1,23 @@
+# hashToPortR
+
+#' Hash a value to a valid port
+#' 
+#' @param hash Character or numeric (quasi-integer) vector of length \code{1}.
+#' @return Integer.
+#' 
+#' @details The implemented hash function is pure but not perfect/injective/
+#' collision free. This is just a R binding to the fabolous \code{hash-to-port}
+#' npm package bundled by \code{mafintosh} based on the marvelous 
+#' \code{hash-index} npm package written by \code{watson}.
+#' 
+#' @seealso \url{https://github.com/mafintosh/hash-to-port} 
+#' \url{https://github.com/watson/hash-index}
+#' 
+#' @export
+hashToPort <- function(hash) {
+  stopifnot(length(hash) == 1L, 
+            is.character(hash) || is.numeric(hash) && hash %% 1L == 0L)
+  ct <- V8::v8()
+  ct$source(file.path(.libPaths()[1], 'hashToPortR', 'bundle.js'))
+  return(ct$call('hashToPort', hash))
+}
